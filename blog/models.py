2abe3b547dfef38c6306a8f183f8822879edbf75
Muhammed Baysan <menesbaysan@gmail.com>
2022-01-08 06:57:27 +0000

    Finish related fields

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,7 @@ from django.utils import timezone
 
 
 class Tag(models.Model):
-    value = models.TextField(max_length=100)
+    value = models.TextField(max_length=100, unique = True)
 
     def __str__(self):
         return self.value
