efed2de4c0a5b5c4e7030fc50ebaf7f8535b08d4
Muhammed Baysan <menesbaysan@gmail.com>
2022-01-08 13:19:47 +0000

    Finish viewsets and routers

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include, re_path
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from blog.api.views import PostList, PostDetail, UserDetail
+from blog.api.views import UserDetail, TagViewSet, PostViewSet
 
 from rest_framework.authtoken import views
 
@@ -9,19 +9,14 @@ from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 import os
 
+from rest_framework.routers import DefaultRouter
+
 urlpatterns = [
-    path("posts/", PostList.as_view(), name="api_post_list"),
-    path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
+    
+    #path("posts/", PostList.as_view(), name="api_post_list"),
+    #path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
     path("users/<str:email>", UserDetail.as_view(), name="api_user_detail"),
-
 ]
-
-urlpatterns += [
-    path("auth/", include("rest_framework.urls")),
-    path("token-auth/", views.obtain_auth_token)
-]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -32,6 +27,16 @@ schema_view = get_schema_view(
     url=f"https://{os.environ.get('CODIO_HOSTNAME')}-8000.codio.io/api/v1/",
     public=True,
 )
+
+router = DefaultRouter()
+router.register("tags", TagViewSet)
+router.register("posts", PostViewSet)
+
+
+
+
+urlpatterns = format_suffix_patterns(urlpatterns)
+
 urlpatterns += [
     path("auth/", include("rest_framework.urls")),
     path("token-auth/", views.obtain_auth_token),
@@ -45,4 +50,6 @@ urlpatterns += [
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
+    path("", include(router.urls)),
+
 ]
