330b84f346da9525758388460a8780cc4088f97e
Muhammed Baysan <menesbaysan@gmail.com>
2022-01-09 08:38:51 +0000

    Finish simplejwt

--- blango/settings.py
+++ blango/settings.py
@@ -16,6 +16,8 @@ import os
 from configurations import Configuration, values
 import dj_database_url
 
+from datetime import timedelta
+
 class Dev(Configuration):
     # Build paths inside the project like this: BASE_DIR / 'subdir'.
     BASE_DIR = Path(__file__).resolve().parent.parent
@@ -214,10 +216,11 @@ class Dev(Configuration):
    # DRF 
     REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.BasicAuthentication",
-        "rest_framework.authentication.SessionAuthentication",
-        "rest_framework.authentication.TokenAuthentication",
-    ],
+            "rest_framework.authentication.BasicAuthentication",
+            "rest_framework.authentication.SessionAuthentication",
+            "rest_framework.authentication.TokenAuthentication",
+            "rest_framework_simplejwt.authentication.JWTAuthentication"
+        ],
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticatedOrReadOnly",
     ],
@@ -250,7 +253,10 @@ class Dev(Configuration):
     }
     
 
-
+    SIMPLE_JWT = {
+        "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+        "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    }
 class Prod(Dev):
     DEBUG = False
     SECRET_KEY = values.SecretValue()
