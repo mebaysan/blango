c67c2d46e0aa31e6bd937e39a39753a92f3be38b
Muhammed Baysan <menesbaysan@gmail.com>
2022-01-09 09:21:41 +0000

    Finish JavaScript functions

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -1,1 +1,18 @@
-alert('NABER');
+function sayHello(name) {
+  if (name === undefined) {
+      console.log('Hello, no name')
+  } else {
+       console.log('Hello, ' + name)
+  }
+}
+
+const name = 'Baysan'  // Put your name here
+
+console.log('Before setTimeout')
+
+setTimeout(() => {
+    sayHello(name)
+  }, 2000
+)
+
+console.log('After setTimeout')
